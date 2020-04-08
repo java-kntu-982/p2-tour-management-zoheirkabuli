378345aa9e76a3030938a7d6a8984db87835a245
maryam <maryam@gmail.com>
2020-04-09 01:07:44 +0430

    Add Date,Tour,TourLeader Classes

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,14 +1,53 @@
 package ir.ac.kntu;
 
 import ir.ac.kntu.maputil.MapUtil;
+import java.util.Scanner;
+
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static char where;
+
     public static void main(String[] args) {
 
 //        Good for showing one location
-        MapUtil.showMap("Shiraz");
-        MapUtil.showMap("@29.6257966,52.5563165,17z");
+//        MapUtil.showMap("Shiraz");
+//        MapUtil.showMap("@29.6257966,52.5563165,17z");
 //        Good for showing two locations
-        MapUtil.showMap("Tehran","Dubai");
+//        MapUtil.showMap("Tehran","Dubai");
+        mainPage();
+    }
+
+
+    public static void mainPage(){
+        print("Enter the number :\n1.Leaders\n2.Tours");
+        where = scanner.next().charAt(0);
+        if (where == '1'){
+            toursPage();
+        }else if (where == '2'){
+
+        }
+    }
+
+    public static void toursPage(){
+        print("Enter The number\n1.All Leaders\n2.Add Leader\n3.Delete Leader\n4.Edit Leader\n5.Search Leader\n6.Back to Main Page");
+        where = scanner.next().charAt(0);
+        if (where == '1'){
+
+        }else if (where == '2'){
+
+        }else if (where == '3'){
+
+        }else if (where == '4'){
+
+        }else if (where == '5'){
+
+        }else if (where == '6'){
+            mainPage();
+        }
+    }
+
+    public static void print(String txt){
+        System.out.println(txt);
     }
 }
---
+++ src/main/java/ir/ac/kntu/TourLeader.java
@@ -0,0 +1,97 @@
+package ir.ac.kntu;
+
+import java.util.ArrayList;
+
+public class TourLeader {
+    private String firstName,lastName,nationalNumber,idNumber;
+    private Date born,recruitment;
+    private boolean isMarried;
+    private ArrayList<String> places;
+
+    public TourLeader(String firstName, String lastName,
+                      String nationalNumber, String idNumber, Date born,
+                      Date recruitment, boolean isMarried, ArrayList<String> places) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalNumber = nationalNumber;
+        this.idNumber = idNumber;
+        this.born = born;
+        this.recruitment = recruitment;
+        this.isMarried = isMarried;
+        this.places = places;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalNumber() {
+        return nationalNumber;
+    }
+
+    public void setNationalNumber(String nationalNumber) {
+        this.nationalNumber = nationalNumber;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public Date getBorn() {
+        return born;
+    }
+
+    public void setBorn(Date born) {
+        this.born = born;
+    }
+
+    public Date getRecruitment() {
+        return recruitment;
+    }
+
+    public void setRecruitment(Date recruitment) {
+        this.recruitment = recruitment;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+
+    public ArrayList<String> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(ArrayList<String> places) {
+        this.places = places;
+    }
+
+    @Override
+    public String toString() {
+        return "TourLeader{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", nationalNumber='" + nationalNumber + '\'' +
+                ", born=" + born +
+                '}';
+    }
+}
